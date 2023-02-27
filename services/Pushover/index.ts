b49ab6fb9525e4f38164d2b9bb7bc8e4ce617f26
“berezhnoi” <ya_sergey@yahoo.com>
2023-02-27 11:56:48 +0300

    init commit

---
+++ services/Pushover/index.ts
@@ -0,0 +1,45 @@
+import {Pushover} from "pushover-js";
+import {Statuses} from "../../constansts";
+import {getNodeInfo} from "../../api";
+
+
+export class ShardeumPushover {
+    private pushover = new Pushover(process.env.PUSHOVER_USER_ID || "", process.env.PUSHOVER_APP_TOKEN || "");
+    private prevStatus = Statuses.Null
+
+    private async sendNotification(message: string) {
+        this.pushover
+            .setSound('cashregister')
+        try {
+            await this.pushover.send('Shardeum', message)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    private notifications = {
+        [Statuses.Active]: async () => await this.sendNotification('Node is active'),
+        [Statuses.StandBy]: async () => await this.sendNotification('Node is standby'),
+        [Statuses.Offline]: async () => await this.sendNotification('Node is not working'),
+    }
+
+    private status = async () => {
+        try {
+            const response = await getNodeInfo()
+            const currentStatus = response?.status ? Statuses.Active : Statuses.StandBy
+            if (currentStatus !== this.prevStatus) {
+                await this.notifications[currentStatus]()
+            }
+            this.prevStatus = currentStatus
+        } catch (e) {
+            if (this.prevStatus !== Statuses.Offline) {
+                await this.notifications[Statuses.Offline]()
+                this.prevStatus = Statuses.Offline
+            }
+        }
+    }
+
+    startPushover() {
+        setInterval(this.status, 1000 * 60)
+    }
+}
