4e29dd38eafd493610a9dcdf86f37b8b6e0e8c77
berezhnoi <S32069257b!!!>
2023-03-24 13:44:14 +0300

    added new functions

--- services/Telegram/index.ts
+++ services/Telegram/index.ts
@@ -14,7 +14,7 @@ const FAUCET_URL = 'https://shardeum-faucet.vercel.app/'
 export const startBot = () => {
 
     //Запускаем шедулер и сохраняем ответ в кеш
-    const scheduler = new Scheduler(5, 5)
+    const scheduler = new Scheduler(30, 30)
     scheduler.start()
 
     bot.start(ctx => {
